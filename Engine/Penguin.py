36148bdfc67533475b0c41aefd21d1c27dfa2691
jjguve5 <jjguve5@gmail.com>
2020-04-29 00:49:38 +0100

    fix login

--- Engine/Penguin.py
+++ Engine/Penguin.py
@@ -3,21 +3,18 @@ from Engine.Filter import Spheniscidae
 class Penguin(Spheniscidae):
     def __init__(self, session, spirit):
         super(Penguin, self).__init__(session, spirit)
-        self.user = None
-        self.throttle = {}
+        self.PID
+        self.objectId
+        self.token
 
-        self.frame = 1
-        self.x, self.y = (0, 0)
-        self.age = 0
-        self.muted = False
-        self.playerString = None
+        self.position
+        self.nextPosition
+        self.nextAttack
 
-        self.table = None
-        self.waddle = None
-        self.gameFinished = True
-
+        self.health
+        self.nextCardXp
         self.logger.info("Penguin class instantiated")
-    
+
     def connectionLost(self, reason):
         #todo add conlost handling
         self.logger.warning("Connection Lost! "+str(reason))
