82e802e474361f2fcc4ad63e82c3a49582ff0fda
klabacher <jvklabacher@gmail.com>
2020-04-29 19:42:01 -0300

    Update GameEngine.py

--- Engine/GameEngine.py
+++ Engine/GameEngine.py
@@ -4,10 +4,12 @@ class GameEngine(object):
         self.fclient = fireclient
         self.wclient = waterclient
         self.sclient = snowclient
+        self.mSpeed = 100
         self.map = None
         print(self.sclient)
         print(self.wclient)
         print(self.fclient)
+        # sly, tank, scrap
     
     def AStag(self, line):
         clients = [fireclient, waterclient, snowclient]
@@ -16,13 +18,63 @@ class GameEngine(object):
 
 
 class Enemy(object, GameEngine):
-    def __init__(self, name, hp, Erange, power):
+    def __init__(self, name, id, hp, Erange, power, round):
         self.name = name
+        self.id = id
         self.hp = hp
         self.range = Erange
         self.power = power
+        self.mSpeed = 100
+        self.round = round
+        if name == "sky":
+            self.design = 1
+            if self.round == 1:
+                self.z = 1
+                self.x = 2
+            elif self.round == 2:
+                self.z = 1
+                self.x = 2
+            elif self.round == 3:
+                self.z = 1
+                self.x = 2
+            else:
+                self.z = 1
+                self.x = 2
+                print("Not supported most than 3 rounds")
+        elif name == "tank":
+            self.design = 2
+            if self.round == 1:
+                self.z = 1
+                self.x = 2
+            elif self.round == 2:
+                self.z = 1
+                self.x = 2
+            elif self.round == 3:
+                self.z = 1
+                self.x = 2
+            else:
+                self.z = 1
+                self.x = 2
+                print("Not supported most than 3 rounds")
+        elif name == "scrap":
+            self.design = 3
+            if self.round == 1:
+                self.z = 1
+                self.x = 2
+            elif self.round == 2:
+                self.z = 1
+                self.x = 2
+            elif self.round == 3:
+                self.z = 1
+                self.x = 2
+            else:
+                self.z = 1
+                self.x = 2
+                print("Not supported most than 3 rounds")
+      
+        self.AStag(f"[O_HERE]|{self.id}|0:{self.}|x|y|128|1|0|0|0||0:1|0|1|0")
 
     def move(self, x, y):
-        tag = f""
-        self.AStag()
+        tag = f"[O_SLIDE]|id do gameobject|{x}|{y}|128|{self.mSpeed}"
+        self.AStag(tag)
 
