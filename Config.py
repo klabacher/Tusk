f93bdaf435ca6e1443a6f7b37e66ac8948f81a10
Klabacher <41272117+klabacher@users.noreply.github.com>
2020-07-09 10:11:33 -0300

    yeahhh

---
+++ Config.py
@@ -0,0 +1,20 @@
+import sys
+config = {
+	"REDIS-DB": {
+		"Address": "localhost",
+		"DB": "0",
+		"Password": None,
+		"Port": "6379"
+	},
+	"Database": {
+        "Address": "localhost",
+		"User":"postgres",
+		"DB": "houdini",
+		"Password": "jv357159",
+		"Port": "5432",
+		"Driver":"psycopg2"
+	},
+	"Mainserver": {
+		"Port": int("7002")
+	}
+}
